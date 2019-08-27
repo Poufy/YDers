7aaaccebe366b64e0de19e92b4fc27cb27b2ec14
root <root@DESKTOP-2KU5GC4.localdomain>
2019-08-27 16:06:19 +0300

    added locus for debugging/fetch-node for fetching from the api

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 - [x] Get the GET/POST/DELETE/UPDATE requests working on /api/teachers
 
+- [x] Connect the api with the count of teachers to be displayed on the landing page dynamically depending on the database
+
 - [ ] Submit the GET requests using the information from the dropdown menus
 
 - [ ] SIGNUP/LOGIN in order to submit the form
--- api/models/Teacher.js
+++ api/models/Teacher.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const teacherSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  name: { type: String, required: true },
-  lastName: { type: String, required: true },
-  subject: { type: String, required: true },
-  location: { type: String, required: true },
+  name: { type: String, required: true, trimmed: true },
+  lastName: { type: String, required: true, trimmed: true },
+  subject: { type: String, required: true, trimmed: true },
+  location: { type: String, required: true, trimmed: true },
   time: { type: Number, required: true }
 });
 
